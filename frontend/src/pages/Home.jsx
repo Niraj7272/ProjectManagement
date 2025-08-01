91e7e62262e41b2eeb23fc5f6eb3fc677e078aee
Niraj7272 <nc72723@gmail.com>
2025-08-01 22:37:47 +0545

    login page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,8 +6,10 @@ const Home = () => {
       <div className="sticky top-0">
         <img src="/src/assets/university.jpg" alt="" className="h-[33rem] w-[81rem] ml-[2rem] bg-red-400" />
       </div>
-      <div className="absolute mt-[-20rem] pl-[35rem]">
-        <h1 className="text-white font-bold text-[2rem]">Welcome to the collage</h1>
+      <div className="absolute mt-[-20rem] ">
+        <h1 className="text-white font-bold text-[2rem] pl-[35rem]">Welcome to the collage</h1>
+        <p className="text-blue-800 font-bold pl-[14rem] pr-[14rem]">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit similique qui quia a, veniam, in reiciendis tempora, ab corrupti accusamus porro obcaecati esse recusandae itaque consectetur consequatur. Laudantium, sunt eum.</p>
+        <button className="bg-white text-blue-800 font-bold h-[2rem] w-[10rem] ml-[38rem] mt-[1rem] cursor-pointer">Login</button>
       </div>
     </div>
   );
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
+    <div className='bg-gray-400 h-[20rem] w-[30rem] ml-[28rem] mt-[8rem] shadow-2xl shadow-black'>
         <form action="">
-            <h1>Login</h1>
-            <input type="email" placeholder='Enter Your Email' /><br /><br />
-            <input type="password" placeholder='Enter your Password' /><br /><br />
-            <button className='bg-blue-800 text-white w-[10rem] cursor-pointer hover:bg-blue-500'>login</button>
+            <h1 className='font-bold text-blue-800 text-[1.7rem] ml-[12rem] pt-[2rem]'>Login</h1>
+            <input type="email" placeholder='Enter Your Email' className='border-1 border-gray-800 h-[2rem] w-[26rem] text-black bg-white ml-[2rem] mt-[3rem]'/><br />
+            <input type="password" placeholder='Enter your Password' className='border-1 border-gray-800 h-[2rem] w-[26rem] text-black bg-white ml-[2rem] mt-[1rem]'/><br /><br />
+            <button className='bg-blue-800 text-white h-[2rem] w-[26rem] ml-[2rem] cursor-pointer hover:bg-blue-500 '>login</button>
         </form>
     </div>
   )
