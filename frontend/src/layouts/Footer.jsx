8c2f13a896b6670784820ba04b2316095efa0b5f
Niraj7272 <nc72723@gmail.com>
2025-08-02 22:11:08 +0545

    footer design

--- frontend/src/layouts/Footer.jsx
+++ frontend/src/layouts/Footer.jsx
@@ -3,6 +3,12 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
+      <div className="bg-yellow-400 h-[5rem] relative top-[7rem] ml-[10rem] mr-[10rem]">
+
+      </div>
+      <div className="bg-gray-400 h-[5rem] ml-[8rem] mr-[12rem]">
+
+      </div>
       <div className="bg-blue-400 h-[20rem] ml-[2rem] mr-[1rem]"></div>
       <div className="bg-gray-400 h-[5rem] ml-[2rem] mr-[1rem]">
         <h1 className="text-white font-bold ml-[34rem] pt-[2rem]">&copy;2026 nc72723@gmail.com</h1>
