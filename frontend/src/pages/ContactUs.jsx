815621a6e347e63d5d4c982d4fc627f5b8817bd7
Niraj7272 <nc72723@gmail.com>
2025-08-03 23:22:36 +0545

    contact us page made

--- frontend/src/pages/ContactUs.jsx
+++ frontend/src/pages/ContactUs.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from "react";
 
 const ContactUs = () => {
   return (
-    <div>ContactUs</div>
-  )
-}
+    <div>
+      <div className="bg-white h-[60rem] border-1 border-blue-500 w-[80rem] ml-[2rem]">
+        <div className="">
+          <img
+            src="src/assets/location1.avif"
+            alt=""
+            className="w-[80rem] h-[30rem] relative"
+          />
+          <h1 className="absolute top-[10rem] ml-[15rem] text-white text-[2.4rem] font-bold">
+            Get In Touch
+          </h1>
+          <p className="absolute top-[14rem] ml-[15rem] w-[30rem] text-gray-700">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+            sapiente eaque alias sed maxime, quaerat quidem cumque ipsam nulla
+            voluptatum doloremque voluptatem labore illo quis minima voluptate
+            facere aperiam sit.
+          </p>
+        </div>
+        <div className="flex">
+        <div>
+          <img
+            src="src/assets/logoimg.jpg"
+            alt=""
+            className="h-[4rem] mt-[19rem] ml-[15rem]"
+          />
+          <p className="text-gray-600 ml-[10rem] w-[20rem] mt-[1rem]">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Perferendis, hic esse. Similique repellat architecto inventore .
+          </p>
+        </div>
+        <div className="mt-[22rem] font-bold text-gray-600 ml-[3rem]">
+          <h1>Products</h1>
+          <h1>About</h1>
+          <h1>Blog</h1>
+          <h1>Contact</h1>
+        </div>
+        <div className="mt-[22rem] font-bold text-gray-600 ml-[2rem]">
+          <h1>Help</h1>
+          <h1>Privacy Policy</h1>
+          <h1>Support</h1>
+          <h1>Terms And Condition</h1>
+        </div>
+        <div className="mt-[22rem] ml-[5rem] font-semibold text-gray-600">
+          <h1>Biratnagar 11 </h1>
+          <h1>Mahendra Chowk,Morang</h1>
+          <div className="flex">
+              <h1>9811111111,</h1>
+          <h2 className="text-blue-700 ml-[0.5rem]">nc72723@gmail.com</h2>
+          </div>
+          <div className="flex">
+            <img src="src/assets/twitter (1).png" alt=""  className="h-[1.7rem]"/>
+            <img src="src/assets/linkedin.png" alt="" className="h-[1.7rem] ml-[1rem]"/>
+            <img src="src/assets/instagram.png" alt="" className="h-[2.2rem] ml-[1rem]"/>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ContactUs
+export default ContactUs;
